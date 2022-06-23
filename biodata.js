fecd53181be8dcc9564e5907d2363ea7547da0d7
Jane Adewole <adewolejane.aj@gmail.com>
2022-06-23 11:12:21 +0100

    updated

--- biodata.js
+++ biodata.js
@@ -2,6 +2,4 @@ var firstName = "Jane O. Adewole";
 var height = "5.6ft";
 var country = "Nigeria";
 
-console.log(firstName);
-console.log(height);
-console.log(country);
+document.write(firstName + "<br>" + height + "<br>" + country);
